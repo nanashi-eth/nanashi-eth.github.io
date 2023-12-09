e5d786a265c161f942b90cd15a8c00c3b1254245
Nanashi <jose98_rbb@hotmail.com>
2023-12-09 16:51:55 +0100

    avance

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
+  var footer = document.querySelector("footer");
+  footer.style.visibility = "hidden";
+
   window.addEventListener("scroll", function () {
-    let offset = window.scrollY;
+    var scrollPosition = window.scrollY;
+    var heroHeight = document.querySelector(".hero").offsetHeight;
+
+    if (scrollPosition > heroHeight / 2) {
+      document.querySelector(".hero").classList.add("active");
+      footer.style.visibility = "visible";
+    } else {
+      document.querySelector(".hero").classList.remove("active");
+      footer.style.visibility = "hidden";
+    }
+  });
+
+  window.addEventListener("scroll", function () {
+    var offset = window.scrollY;
     document.querySelector(".hero").style.backgroundPositionY =
       offset * 0.7 + "px";
   });
-});
 
-document.addEventListener("DOMContentLoaded", function () {
   window.addEventListener("scroll", function () {
-    let scrollProgress =
+    var scrollProgress =
       window.scrollY / (document.body.scrollHeight - window.innerHeight);
-    let rotation = 360 * scrollProgress;
+    var rotation = 360 * scrollProgress;
     document.querySelector(".pen").style.transform = `rotate(${rotation}deg)`;
   });
 });
